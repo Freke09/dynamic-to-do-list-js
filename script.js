ff5178799d2bf6b693b6ddbd0314ecd7b56a85ed
Ndifreke Ndue <ndifrekeh@gmail.com>
2025-08-03 21:47:39 +0100

    Modified script.js to implement localStorage for the To-Do List

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
+    loadTasks();
+    function loadTasks() {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+    storedTasks.forEach(taskText => addTask(taskText, false)); // 'false' indicates not to save again to Local Storage
+}
 
     const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
 
-    function addTask() {
+    function addTask(taskText, save = true) {
         const taskText = taskInput.value.trim();
 
         if(!taskText){
@@ -26,6 +31,11 @@ document.addEventListener("DOMContentLoaded", function() {
             taskInput.value = '';
         }
 
+        if (save) {
+        const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+        storedTasks.push(taskText);
+        localStorage.setItem('tasks', JSON.stringify(storedTasks));
+    }
         
     }
     addButton.addEventListener('click', addTask);
